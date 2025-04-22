e2c0bc54e18750cff473e1237b860b2009c3eab1
atomjin <kazukiryouto@gmail.com>
2025-04-22 15:54:46 +0700

    change url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = "http://localhost:8000"; // Change this if your backend is running on another port
+const API_BASE_URL = "https://streamlabs.com/api/v2.0/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=donations.read+donations.create"; // Change this if your backend is running on another port
 
 export default API_BASE_URL;
